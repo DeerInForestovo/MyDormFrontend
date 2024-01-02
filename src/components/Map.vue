5be44ec2708241fd971eba8647f625ef0f8a1268
bcright <1256514466@qq.com>
2024-01-02 22:53:50 +0800

    delete_some

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,11 +1,10 @@
 <script setup>
-import {ref, onMounted, nextTick, computed} from 'vue'
+import {ref, onMounted} from 'vue'
 import axiosFunctions from "@/utils/api";
 import {useStore} from 'vuex';
 
 const store = useStore();
 const zoneId = store.state.zoneId;
-// import mapSvgFile from './MapComponents/MapSvg.svg'
 
 const image = ref(null)
 const canvas = ref(null);
@@ -170,30 +169,13 @@ export default {
     <canvas ref="canvas" class="canvas-overlay"></canvas>
     <map name="image-map">
       <area target="_self"  :coords="building.coordinates" shape="poly" v-for="building in buildings"
-            :key="building.buildingId" @click="handleAreaClick(building.buildingId)">
-      <!--      <el-popover-->
-      <!--          title="二期宿舍11栋"-->
-      <!--          width="200"-->
-      <!--          trigger="hover"-->
-      <!--          content="这是弹出信息的内容">-->
-      <!--        <area id="popover-area1" target="_self" alt="area2-11" href="" coords="733,1608,739,1627,796,1614,796,1594"-->
-      <!--              shape="poly">-->
-      <!--      </el-popover>-->
+            :key="building.buildingId" @click="handleAreaClick(building.buildingId)" alt="">
     </map>
   </div>
-
-  <!--  <el-popover ref="popover" placement="top" trigger="manual" v-show="popoverVisible">-->
-  <!--    <template #default>-->
-  <!--      &lt;!&ndash; 弹出框内容 &ndash;&gt;-->
-  <!--      <div>这是一个弹出框</div>-->
-  <!--    </template>-->
-  <!--  </el-popover>-->
-
 </template>
 
 
 <style scoped>
-
 .wrap {
   position: relative;
   width: 100vw;
@@ -203,99 +185,11 @@ export default {
   align-items: center;
   justify-content: center;
 }
-
 .img, .canvas-overlay {
   position: absolute;
   cursor: move;
 }
-
-
 .canvas-overlay {
   pointer-events: none; /* 确保 Canvas 不阻止对图片的交互 */
 }
-
 </style>
-<!--<script>-->
-<!--export default {-->
-<!--  methods: {-->
-<!--    // 拖动图片-->
-<!--    move(e) {-->
-<!--      e.preventDefault();-->
-<!--      // 获取元素-->
-<!--      var left = document.querySelector(".left");-->
-<!--      var img = document.querySelector(".img");-->
-<!--      var x = e.pageX - img.offsetLeft;-->
-<!--      var y = e.pageY - img.offsetTop;-->
-<!--      // 添加鼠标移动事件-->
-<!--      left.addEventListener("mousemove", move);-->
-
-<!--      function move(e) {-->
-<!--        img.style.left = e.pageX - x + "px";-->
-<!--        img.style.top = e.pageY - y + "px";-->
-<!--      }-->
-
-<!--      // 添加鼠标抬起事件，鼠标抬起，将事件移除-->
-<!--      img.addEventListener("mouseup", function () {-->
-<!--        left.removeEventListener("mousemove", move);-->
-<!--      });-->
-<!--      // 鼠标离开父级元素，把事件移除-->
-<!--      left.addEventListener("mouseout", function () {-->
-<!--        left.removeEventListener("mousemove", move);-->
-<!--      });-->
-<!--    },-->
-<!--    // 缩放图片-->
-<!--    rollImg() {-->
-<!--      /* 获取当前页面的缩放比 若未设置zoom缩放比，则为默认100%，即1，原图大小 */-->
-
-<!--      var zoom = parseInt(this.$refs.imgDiv.style.zoom) || 100;-->
-<!--      /* event.wheelDelta 获取滚轮滚动值并将滚动值叠加给缩放比zoom wheelDelta统一为±120，其中正数表示为向上滚动，负数表示向下滚动 */-->
-
-<!--      zoom += event.wheelDelta / 12;-->
-<!--      /* 最小范围 和 最大范围 的图片缩放尺度 */-->
-
-<!--      if (zoom >= 80 && zoom < 500) {-->
-<!--        this.$refs.imgDiv.style.zoom = zoom + "%";-->
-<!--      }-->
-<!--      return false;-->
-<!--    }-->
-<!--  }-->
-<!--};-->
-<!--<script>-->
-<!--export default {-->
-<!--  components: {-->
-<!--    'el-popover': Popover-->
-<!--  },-->
-<!--  mounted() {-->
-<!--    // 获取区域元素-->
-<!--    const area = document.getElementById("popover-area1");-->
-
-<!--    // 添加点击事件监听器-->
-<!--    area.addEventListener("click", (event) => {-->
-<!--      event.preventDefault(); // 阻止默认链接行为-->
-
-<!--      // 在这里触发Popover组件的显示-->
-<!--      this.$refs.popover.doShow();-->
-<!--    });-->
-<!--  }-->
-<!--};-->
-<!--</script>-->
-
-<!--<script>-->
-<!--  // 获取区域元素-->
-<!--    // 在这里编写弹出气泡卡片的逻辑-->
-<!--    var area = document.getElementById("popover-area1");-->
-
-<!--    // 添加点击事件监听器-->
-<!--    area.addEventListener("click", function(event) {-->
-<!--      event.preventDefault(); // 阻止默认链接行为-->
-
-<!--      // 可以使用 Element UI 中的 Popover 组件或其他类似的库来实现-->
-
-<!--    // 例如，使用 Element UI 的 Popover 组件：-->
-<!--    var popoverContent = "这是气泡卡片的内容";-->
-<!--    var popoverElement = document.createElement("div");-->
-<!--    popoverElement.innerHTML = popoverContent;-->
-<!--    area.appendChild(popoverElement);-->
-<!--    // 这里假设你已经引入了 Element UI 相关的样式和脚本-->
-<!--  });-->
-<!--</script>-->
--- src/components/RoomComponents/Buildings.vue
+++ src/components/RoomComponents/Buildings.vue
@@ -74,41 +74,6 @@ export default {
       console.error('Failed to fetch rooms:', error);
     }
   },
-  // mounted() {
-  //   axiosFunctions.methods.getRoomFromZone(this.$store.state.zoneId)
-  //       .then((response) => {
-  //         console.log(response)
-  //         // 从房间信息中提取建筑信息
-  //         const rooms = response.data;
-  //
-  //         // 使用reduce方法来构建一个以buildingId为键，building对象为值的映射
-  //         const buildingMap = rooms.reduce((acc, room) => {
-  //           // 如果此buildingId尚未在累加器对象中创建，则创建它
-  //           if (!acc[room.buildingId]) {
-  //             acc[room.buildingId] = {
-  //               buildingId: room.buildingId,
-  //               buildingName: room.buildingName,
-  //               // 假设description是一个静态的字符串，或者您可以从room对象中获取它
-  //               description: "Description of " + room.buildingName,
-  //             };
-  //           }
-  //           return acc;
-  //         }, {});
-  //         console.log(buildingMap);
-  //         // 从建筑映射对象中提取值以创建建筑数组
-  //         this.buildings = Object.values(buildingMap);
-  //
-  //         this.cards = this.buildings.map(
-  //             (building) => ({
-  //               id: building.buildingId,
-  //               title: building.buildingName,
-  //               content: building.description,
-  //             })
-  //         )
-  //       }).catch((response) => {
-  //     console.log(response)
-  //   })
-  // },
 }
 </script>
 
